210be3a95191d9cc4fa5eebc4ef537fea7420fad
SFENCE <sfence.software@gmail.com>
2025-03-18 16:57:00 +0100

    Try update Xcode plist.

--- sdk.sh
+++ sdk.sh
@@ -10,6 +10,17 @@ download_macos_archive() {
 	fi
 }
 
+update_xcode_plist() {
+	plist=$1
+	minsdk=#2
+
+	cp $plist Info.plist
+	plutil -convert xml1 Info.plist
+	sed -i '' "/<key>MinimumSDKVersion<\/key>/{n;s/<string>.*<\/string>/<string>$minsdk<\/string>/;}" Info.plist
+	plutil -convert binary1 /path/to/Info.plist
+	cp Info.plist $plist
+}
+
 install_macos_sdk() {
 	osver=$1
 	xcodever=$2
@@ -34,12 +45,14 @@ install_macos_sdk() {
 					d6604578f4ee3090d1c3efce1e5c336ecfd7be345d046c729189d631ea3b8ec6
 			tar -xf $downdir/SDK.tar.bz2
 			mv MacOSX11.3.sdk $sysroot
+			update_xcode_plist "/Applications/$xcodeapp/Contents/Developer/Platforms/MacOSX.platform/Info.plist" $osver
 		elif [[ "$osver" == "12.3" ]]; then
 			download_macos_archive $downdir/SDK.tar.bz2 \
 					https://github.com/alexey-lysiuk/macos-sdk/releases/download/12.3/MacOSX12.3.tar.bz2 \
 					91c03be5399be04d8f6b773da13045525e01298c1dfff273b4e1f1e904ee5484
 			tar -xf $downdir/SDK.tar.bz2
 			mv MacOSX12.3.sdk $sysroot
+			update_xcode_plist "/Applications/$xcodeapp/Contents/Developer/Platforms/MacOSX.platform/Info.plist" $osver
 		elif [[ "$osver" == "13.3" ]]; then
 			download_macos_archive $downdir/SDK.tar.bz2 \
 					https://github.com/alexey-lysiuk/macos-sdk/releases/download/13.3/MacOSX13.3.tar.bz2 \
